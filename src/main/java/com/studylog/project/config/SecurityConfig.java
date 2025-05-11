166cec237bcf06ea380d1243501e15d1c2de2553
pponya11 <kah1722291@gmail.com>
2025-05-11 23:32:40 +0900

    feat: Swagger 허용 + 기본 Security 설정 추가

---
+++ src/main/java/com/studylog/project/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.studylog.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration //Config 파일로 설정
+@EnableWebSecurity //WebSecurity 활성화
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        (auth) -> auth
+                                .requestMatchers(
+                                        "/",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources/**",
+                                        "/webjars/**")
+                                .permitAll()
+                                .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable()); //POST 테스트 할 수 있게 CSRF 끔
+        return http.build();
+    }
+}
