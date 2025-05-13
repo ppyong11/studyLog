589ab47784d0e32a2bbe22a4fcdd3d8e8120ee65
pponya11 <kah1722291@gmail.com>
2025-05-13 15:07:29 +0900

    feat: runtime 에러 처리 전역 핸들러 설정

---
+++ src/main/java/com/studylog/project/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.studylog.project.exception;
+
+import com.studylog.project.dto.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity handleRuntime(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiResponse(false, "서버 오류 발생: "+e.getMessage())); //오류 내용 축약해서 출력
+    }
+}
