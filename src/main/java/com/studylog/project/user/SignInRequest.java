b9bda2e6d5444aaba53dfd4c1d5c901222ec29b4
pponya11 <kah1722291@gmail.com>
2025-05-16 16:25:14 +0900

    feat: 회원가입 요청 DTO

--- src/main/java/com/studylog/project/user/SignInRequest.java
+++ src/main/java/com/studylog/project/user/SignInRequest.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
-public class UserRequest {
+public class SignInRequest {
     @Pattern(regexp = "^[a-zA-Z0-9]{4,12}$", message = "아이디는 4~12자 영문 또는 숫자여야 합니다.")
     private String id;
     @Pattern(regexp = "^[a-zA-Z0-9]{6,20}$", message = "비밀번호는 6~20자 영문 또는 숫자여야 합니다.")
