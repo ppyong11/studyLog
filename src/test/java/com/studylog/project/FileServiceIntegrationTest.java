7b136ba3241c53a6ebdaccf318fb036478f5ea50
pponya11 <kah1722291@gmail.com>
2025-11-19 19:22:33 +0900

    style: 안 쓰는 import 삭제

--- src/test/java/com/studylog/project/FileServiceIntegrationTest.java
+++ src/test/java/com/studylog/project/FileServiceIntegrationTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
