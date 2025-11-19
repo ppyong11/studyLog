455fee28989c8ca26fecf70d773afdeaa4e50f65
pponya11 <kah1722291@gmail.com>
2025-11-20 04:15:43 +0900

    testRefactor: 코드 간결화

--- src/test/java/com/studylog/project/FileServiceIntegrationTest.java
+++ src/test/java/com/studylog/project/FileServiceIntegrationTest.java
@@ -7,6 +7,10 @@ import com.studylog.project.category.CategoryRepository;
 import com.studylog.project.file.FileEntity;
 import com.studylog.project.file.FileRepository;
 import com.studylog.project.file.FileService;
+import com.studylog.project.fixture.BoardFixture;
+import com.studylog.project.fixture.CategoryFixture;
+import com.studylog.project.fixture.FileFixture;
+import com.studylog.project.fixture.UserFixture;
 import com.studylog.project.global.exception.CustomException;
 import com.studylog.project.user.UserEntity;
 import com.studylog.project.user.UserRepository;
@@ -39,57 +43,10 @@ public class FileServiceIntegrationTest {
     @Autowired
     private BoardRepository boardRepository;
 
-
-    private UserEntity createAndSaveUser(String suffix) {
-        UserEntity user = UserEntity.builder()
-                .id("test" + suffix)
-                .pw("1234567")
-                .nickname("테스트유저" + suffix)
-                .email("test" + suffix + "@gmail.com")
-                .build();
-
-        return userRepository.save(user); // 저장된 객체 반환
-    }
-
-    private FileEntity createAndSaveFile(UserEntity user, BoardEntity board, String draftId) {
-        FileEntity file = FileEntity.builder()
-                .user(user)
-                .board(board)
-                .draft(draftId)
-                .size(66756L)
-                .path("test/test.jpg")
-                .originalName("test.png")
-                .name("test.png")
-                .type("image/png")
-                .build();
-
-        return fileRepository.save(file);
-    }
-
-    private BoardEntity createAndSaveBoard(UserEntity user) {
-        CategoryEntity category = CategoryEntity.builder()
-                .user_id(user)
-                .name("테스트")
-                .bgColor("#F7F7F7")
-                .textColor("#484848")
-                .build();
-
-        categoryRepository.save(category);
-
-        BoardEntity board = BoardEntity.builder()
-                .user(user)
-                .category(category)
-                .title("테스트 게시글")
-                .content("테스트")
-                .build();
-
-        return boardRepository.save(board);
-    }
-
     @Test
     void 임시_값_미일치로_파일_삭제_실패() {
-        UserEntity user = createAndSaveUser("3");
-        FileEntity file = createAndSaveFile(user, null, "test-123");
+        UserEntity user = UserFixture.createAndSaveUser("3", userRepository);
+        FileEntity file = FileFixture.createAndSaveFile(user, null, "test-123", fileRepository);
 
         assertThatCode(() -> fileService.deleteTempMeta(file.getId(), "test-121", user))
                 .isInstanceOf(CustomException.class);
@@ -97,10 +54,10 @@ public class FileServiceIntegrationTest {
 
     @Test
     void 유저_미일치로_파일_삭제_실패() {
-        UserEntity user1 = createAndSaveUser("3");
-        UserEntity user2 = createAndSaveUser("4");
+        UserEntity user1 = UserFixture.createAndSaveUser("3", userRepository);
+        UserEntity user2 = UserFixture.createAndSaveUser("4", userRepository);
 
-        FileEntity file = createAndSaveFile(user1, null, "test-123");
+        FileEntity file = FileFixture.createAndSaveFile(user1, null, "test-123", fileRepository);
 
         assertThatCode(() -> fileService.deleteTempMeta(file.getId(), "test-123", user2))
                 .isInstanceOf(CustomException.class);
@@ -108,12 +65,13 @@ public class FileServiceIntegrationTest {
 
     @Test
     void 게시글_파일_매핑_성공() {
-        UserEntity user1 = createAndSaveUser("3");
+        UserEntity user1 = UserFixture.createAndSaveUser("3", userRepository);
 
-        FileEntity file1 = createAndSaveFile(user1, null, "test-123");
-        FileEntity file2 = createAndSaveFile(user1, null, "test-123");
+        FileEntity file1 = FileFixture.createAndSaveFile(user1, null, "test-123",fileRepository);
+        FileEntity file2 = FileFixture.createAndSaveFile(user1, null, "test-123",fileRepository);
 
-        BoardEntity board = createAndSaveBoard(user1);
+        CategoryEntity category = CategoryFixture.createAndSaveCategory(user1, categoryRepository);
+        BoardEntity board = BoardFixture.createAndSaveBoard(user1, category, boardRepository);
 
         fileService.attachDraftFilesToBoard(user1, board, "test-123");
 
@@ -126,9 +84,11 @@ public class FileServiceIntegrationTest {
 
     @Test
     void 게시글이_매핑된_파일_삭제() {
-        UserEntity user1 = createAndSaveUser("3");
-        BoardEntity board = createAndSaveBoard(user1);
-        FileEntity file1 = createAndSaveFile(user1, board, null);
+        UserEntity user1 = UserFixture.createAndSaveUser("3", userRepository);
+        CategoryEntity category = CategoryFixture.createAndSaveCategory(user1, categoryRepository);
+        BoardEntity board = BoardFixture.createAndSaveBoard(user1, category, boardRepository);
+
+        FileEntity file1 = FileFixture.createAndSaveFile(user1, board, null, fileRepository);
 
         // 삭제 전
         Optional<FileEntity> deleted = fileRepository.findById(file1.getId());
